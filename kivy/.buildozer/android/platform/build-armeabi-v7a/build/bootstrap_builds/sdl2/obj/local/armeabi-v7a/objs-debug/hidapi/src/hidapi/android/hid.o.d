14279edb2aafc5bd201ad410e07aceaba2ff92f8
Vladimir <arazkov@ya.ru>
2021-05-20 23:21:16 +0300

    remove kivy repository

--- kivy/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/hidapi/src/hidapi/android/hid.o.d
+++
@@ -1,5 +0,0 @@
-/home/ara/PycharmProjects/kivy/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/hidapi/src/hidapi/android/hid.o: \
-  /home/ara/PycharmProjects/kivy/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/hid.cpp \
-  /home/ara/PycharmProjects/kivy/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/../hidapi/hidapi.h
-
-/home/ara/PycharmProjects/kivy/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/../hidapi/hidapi.h:
--- kivy/.buildozer/android/platform/build-armeabi-v7a/dists/mymobilecalc__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/ara/PycharmProjects/kivy/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/ara/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.c \
-  /home/ara/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h
-
-/home/ara/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h:
